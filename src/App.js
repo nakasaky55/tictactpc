15d45ca8690298107e3debef0b759e982171be12
Nakasaky55 <themaskmen1996@gmail.com>
2019-10-23 17:33:37 +0700

    update API

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React, { useState } from "react";
-import { Container, Navbar, Row, Col, Button } from "react-bootstrap";
+import React, { useState, useEffect } from "react";
+import { Container, Navbar, Row, Col, Button, Table } from "react-bootstrap";
+import FacebookLogin from "react-facebook-login";
 import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   const [board, setBoard] = useState(
-    new Array(9).fill({ value: null, color: null })
+    new Array(9).fill({ value: null, color: "X-hover" })
   );
 
   const setMove = (arr, index) => {
@@ -16,64 +17,126 @@ function App() {
 
   const [historyMove, setHistoryMove] = useState(new Array().fill(null));
   const [gameHistory, setGameHistory] = useState(new Array().fill(null));
-  console.log(gameHistory);
+
+  //user's data from fb
+  const [userData, setUserData] = useState(null);
+
+  //store data get from API
+  const [gameData, setGameData] = useState(null);
+
+  //set data to API
+  const postApi = async () => {
+    let data = new URLSearchParams();
+    data.append("player", userData.name);
+    data.append("score", -Infinity);
+    const url = `https://ftw-highscores.herokuapp.com/tictactoe-dev`;
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded"
+      },
+      body: data.toString(),
+      json: true
+    });
+
+    console.log("response", response);
+    getAPI();
+  };
+
+  //get data from API
+  const getAPI = async () => {
+    const url = "https://ftw-highscores.herokuapp.com/tictactoe-dev";
+    const response = await fetch(url);
+    const jsonResult = await response.json();
+
+    setGameData(jsonResult.items);
+  };
+
+  const responseFacebook = resp => {
+    console.log(resp);
+    setUserData({
+      name: resp.name,
+      email: resp.email,
+      picture: resp.picture.data.url
+    });
+  };
+
+  useEffect(() => {
+    getAPI();
+  }, []);
+
   return (
     <>
-      <Navbar bg="dark" variant="dark">
-        <Navbar.Brand href="#home">
-          <img
-            alt=""
-            src="/logo.svg"
-            width="30"
-            height="30"
-            className="d-inline-block align-top"
-          />{" "}
-          React Bootstrap
-        </Navbar.Brand>
-      </Navbar>
-      <Container fluid="true">
-        <Row>
-          <Col lg={8}>
-            <Board
-              board={board}
-              setBoard={setBoard}
-              setHistoryMove={setHistoryMove}
-              historyMove={historyMove}
-              gameHistory={gameHistory}
-              setGameHistory={setGameHistory}
-            />
-          </Col>
-          <Col lg={4}>
+      {!userData ? (
+        <FacebookLogin
+          appId="2149915191977471"
+          // autoLoad={true}
+          fields="name,email,picture"
+          // onClick={componentClicked}
+          callback={resp => responseFacebook(resp)}
+        />
+      ) : (
+        <>
+          <Navbar bg="dark" variant="dark">
+            <Navbar.Brand href="#home">
+              <img
+                alt=""
+                src="/logo.svg"
+                width="30"
+                height="30"
+                className="d-inline-block align-top"
+              />
+              Welcome {userData.name}{" "}
+              <img src={userData.picture} roundedCircle></img>
+            </Navbar.Brand>
+          </Navbar>
+          <Container fluid="true">
             <Row>
-              <h1>History move</h1>
-              <ol>
-                {historyMove.map((item, idx) => {
-                  return (
-                    <div>
-                      <li>
-                        <Button
-                          onClick={() => setMove(item, idx)}
-                          variant="outline-dark"
-                        >
-                          Move {idx + 1}
-                        </Button>
-                      </li>
-                    </div>
-                  );
-                })}
-              </ol>
+              <Col lg={8}>
+                <Board
+                  board={board}
+                  setBoard={setBoard}
+                  setHistoryMove={setHistoryMove}
+                  historyMove={historyMove}
+                  gameHistory={gameHistory}
+                  setGameHistory={setGameHistory}
+                  gameData={gameData && gameData}
+                  postApi={postApi}
+                />
+              </Col>
+              <Col lg={4}>
+                <Row>
+                  <h1>History move</h1>
+                  <ol>
+                    {historyMove.map((item, idx) => {
+                      return (
+                        <div>
+                          <li>
+                            <Button
+                              onClick={() => setMove(item, idx)}
+                              className="custom-btn"
+                            >
+                              Move {idx + 1}
+                            </Button>
+                          </li>
+                        </div>
+                      );
+                    })}
+                  </ol>
+                </Row>
+                <Row>
+                  <h1>Game History</h1>
+                  <ul>
+                    {gameHistory.map(item => {
+                      return <li>{item} is winner</li>;
+                    })}
+                  </ul>
+                </Row>
+              </Col>
             </Row>
-            <Row>
-              <h1>Game History</h1>
-              <ul>
-                {gameHistory.map(item => {
-                  return <li>{item} is winner</li>;
-                })}
-              </ul>
-            </Row>
-          </Col>
-        </Row>
-      </Container>
+          </Container>
+        </>
+      )}
     </>
   );
 }
@@ -88,7 +151,7 @@ function Board(props) {
 
   const Reset = () => {
     setIsOver(false);
-    props.setBoard(new Array(9).fill({ value: null, color: null }));
+    props.setBoard(new Array(9).fill({ value: null, color: "X-hover" }));
     setResult(null);
     props.setHistoryMove(new Array());
   };
@@ -117,10 +180,12 @@ function Board(props) {
         arr[c].color = "black" + arr[c].value;
         let newGamehistory = props.gameHistory;
         newGamehistory.push(arr[a].value);
-        console.log(newGamehistory);
+
         props.setGameHistory(newGamehistory);
+        props.setBoard(arr);
         setResult(`${arr[a].value} is the winner`);
         setIsOver(true);
+        props.postApi();
         return true;
       }
     }
@@ -137,31 +202,71 @@ function Board(props) {
 
     if (!dataBoard[index].value)
       dataBoard[index] = { value: check % 2 ? "X" : "O", color: null };
-    // //if a square has X or O inside, prevent user from changing its value
     else return;
+    const newDataBoard = dataBoard.map(item => {
+      return {
+        value: item.value,
+        color: item.value === null ? (check % 2 ? "O-hover" : "X-hover") : null
+      };
+    });
+    // //if a square has X or O inside, prevent user from changing its value
+    console.log("new", newDataBoard);
     if (check == 1) setResult("draw");
-    props.setBoard(dataBoard);
+    props.setBoard(newDataBoard);
     let tempArray = props.historyMove;
     tempArray.push(dataBoard);
     props.setHistoryMove(tempArray);
-    if (winCondition(dataBoard)) setIsOver(true);
+    if (winCondition(dataBoard)) {
+      setIsOver(true);
+    }
   };
 
   return (
-    <div className="board">
-      {props.board.map((item, idx) => {
-        return (
-          <Square key={idx} item={item} index={idx} handleClick={handleClick} />
-        );
-      })}
-      <div>
-        <div>Result {result && result}</div>
-        <div>
-          <Button variant="outline-info" onClick={() => Reset()}>
-            Reset game
-          </Button>
-        </div>
+    <div className="board-container">
+      <div className="result">Result {result && result}</div>
+      <div className="board">
+        {props.board.map((item, idx) => {
+          return (
+            <Square
+              key={idx}
+              item={item}
+              index={idx}
+              handleClick={handleClick}
+              onMouseOver={() => console.log("val")}
+            />
+          );
+        })}
       </div>
+      <div>
+        <Button className="custom-btn reset-btn" onClick={() => Reset()}>
+          Reset game
+        </Button>
+      </div>
+      <div>
+        <h2 className="highscore">High score</h2>
+
+        <Table bordered className="custom-table">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Name</th>
+              <th>Score</th>
+            </tr>
+          </thead>
+          <tbody>
+            {props.gameData.map((item, index) => {
+              return (
+                <tr className="row-hover">
+                  <td>{index}</td>
+                  <td>{item.player}</td>
+                  <td>{item.score ? item.score : "Infinity"}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </Table>
+      </div>
+      <div></div>
     </div>
   );
 }
